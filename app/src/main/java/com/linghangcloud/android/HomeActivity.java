453607cdf91a4c55b2522405d53a693ed966a325
Dragon <332062922@qq.com>
2019-10-14 22:44:41 +0800

    first commit

--- app/src/main/java/com/linghangcloud/android/HomeActivity.java
+++ app/src/main/java/com/linghangcloud/android/HomeActivity.java
@@ -6,10 +6,11 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.linghangcloud.android.TaskDetail.TaskDetailActivity;
 
 public class HomeActivity extends AppCompatActivity {
 private Button menu;
